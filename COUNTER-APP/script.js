f9caa423fff3ebc141650196682ab542bc36d9b1
Mohd Aasim <masimshaikh8976@gmail.com>
2026-02-09 20:08:16 +0530

    working

--- COUNTER-APP/script.js
+++ COUNTER-APP/script.js
@@ -1,21 +1,21 @@
 let count = 0;
 
-const counter = document.getElemetById("counter");
+const counter = document.getElementById("counter");
 const decrease = document.getElementById("decrease");
 const increase = document.getElementById("increase");
 const reset = document.getElementById("reset");
 
 increase.onclick = () =>{
-    count ++;
+    count++;
     counter.textContent = count;
-}
+};
 
 decrease.onclick = () =>{
     count--;
     counter.textContent = count;
-}
+};
 
 reset.onclick = () =>{
     count = 0;
     counter.textContent = count;
-}
+};
