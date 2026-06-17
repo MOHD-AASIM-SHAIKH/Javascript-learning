d78a70dc04b6073044a735f2192dd109d08de38a
Mohd Aasim <mohdaasimsky0@gmail.com>
2026-06-17 20:02:04 +0530

    made hashing aand try to compare it with password of login

--- index.js
+++ index.js
@@ -7,4 +7,24 @@ bcrypt.genSalt(saltRounds, (err, salt) => {
     return;
   }
 });
+const password = 'MOHDaasim';
+bcrypt.hash(password, saltRounds, (err, hash) => {
+  if (err) {
+    console.error('Error hashing password:', err);
+    return;
+  }
+  console.log('Hashed password:', hash);
+  const storedhash = hash;
+bcrypt.compare(loginpass,storedhash, (err, result) => {
+    if(err){
+        console.error('Error comparing password:', err);
+        return;
+    }
+    if(result){
+        console.log('Password is correct');
+    }else{
+        console.log('Password is incorrect');
+    }
+});
 
+});
